349bbe527cf61083c1b373ec96f08d3ce76938b0
Deo's Macbook Pro <macbookpro@Deoss-MBP-2.Dlink>
2023-10-27 10:49:09 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIg6zMjfb-0iwXMk-ESDk4O-asEFEOcU0",
+    authDomain: "netflix-clone-6bf6c.firebaseapp.com",
+    projectId: "netflix-clone-6bf6c",
+    storageBucket: "netflix-clone-6bf6c.appspot.com",
+    messagingSenderId: "41913636374",
+    appId: "1:41913636374:web:77e5b2a43b11f9e661c737"
+  };
